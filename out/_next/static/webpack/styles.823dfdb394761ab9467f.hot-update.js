1ffbc1ec9e49444a0b9d46b8548648eace7d6bc9
TseHang <a54383813@gmail.com>
2019-03-29 00:51:07 +0800

    delete out

--- out/_next/static/webpack/styles.823dfdb394761ab9467f.hot-update.js
+++
@@ -1,6 +0,0 @@
-webpackHotUpdate('styles', {
-  /***/ './node_modules/antd/lib/avatar/style/index.less': false,
-
-  /***/ 7: false,
-});
-//# sourceMappingURL=styles.823dfdb394761ab9467f.hot-update.js.map
