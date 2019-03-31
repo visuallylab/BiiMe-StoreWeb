e82f35cf01e054ac467c7fc5157f43f86e86ff64
TseHang <a54383813@gmail.com>
2019-04-01 00:37:05 +0800

    remove out

--- out/_next/static/chunks/styles.5258a82782b985301ce1.js
+++
@@ -1,9 +0,0 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([
-  [2],
-  {
-    223: function(n, o, c) {},
-    226: function(n, o, c) {},
-    236: function(n, o, c) {},
-    237: function(n, o, c) {},
-  },
-]);
